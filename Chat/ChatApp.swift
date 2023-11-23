4fdb15eed22d016514186fd5b316851d4c50e17b
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-11-23 09:04:49 +0000

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Miguel Alejandro Correa Avila on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
