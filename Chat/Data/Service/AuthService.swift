2557b8341c55d56e8e66219ef38b7f7676b52de1
josedaniel-chinea-marrero_pwcit <josedaniel.chinea.marrero@pwc.com>
2023-11-23 13:16:26 +0000

    Correction of authService

--- Chat/Data/Service/AuthService.swift
+++ Chat/Data/Service/AuthService.swift
@@ -6,49 +6,58 @@
 //
 
 import Firebase
-import FirebaseAuth
+import FirebaseFirestore
+import Combine
+import SwiftUI
 
+/*
 class AuthService {
-    static let shared = AuthService()
+    @Published var user: User?
+    @Published var userUID: String?
+    @Published var userName: String? = nil
+    @Published var hasFetchedUserName = false
+    
+    
+    func signIn(email: String, password: String) {
+         Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+             if let user = result?.user {
+                 self?.user = user
+                 self?.couponViewModel.loadUserCoupons(userID: user.uid)
+                 self?.userUID = user.uid
+                 self?.currentUserName()
+             } else if let error = error {
+                 print("Error al iniciar sesión:", error.localizedDescription)
+             }
+         }
+     }
 
-    private init() {}
 
-    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
-        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
+     func signUp(email: String, password: String, name: String) {
+         let db = Firestore.firestore()
+         Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+             if let user = result?.user {
+                 self?.user = user
+                 db.collection("users").document(user.uid).setData([
+                     "name": name,
+                     "email": email
+                 ]) { error in
+                     if let error = error {
+                         print("Error al crear el documento del usuario: \(error)")
+                     } else {
+                         self?.couponViewModel.addDefaultCouponsToUser(userID: user.uid)
+                         self?.couponViewModel.loadUserCoupons(userID: user.uid)
+                         self?.currentUserName()
+                     }
+                 }
+             }
+         }
+     }
 
-            guard let user = authResult?.user else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No se pudo obtener el usuario."])))
-                return
-            }
 
-            completion(.success(user))
-        }
-    }
-
-    func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-
-            guard let user = authResult?.user else {
-                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No se pudo crear el usuario."])))
-                return
-            }
-
-            completion(.success(user))
-        }
-    }
-
-    func signOut() throws {
-        try Auth.auth().signOut()
-    }
-
-    // Agrega más funciones según sea necesario, como restablecer contraseña, verificar email, etc.
+     func signOut() {
+         try? Auth.auth().signOut()
+         user = nil
+         hasFetchedUserName = false
+     }
 }
-
+*/
