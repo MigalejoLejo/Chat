c6865df9cea25b4d606d7639ecead7c4dce62088
Migalejo <migalejo85@gmail.com>
2023-11-27 11:49:40 +0000

    develop ready

---
+++ Chat/Presentation/Views/ChatContactList.swift
@@ -0,0 +1,18 @@
+//
+//  ChatContactList.swift
+//  Chat
+//
+//  Created by Miguel Alejandro Correa Avila on 27/11/23.
+//
+
+import SwiftUI
+
+struct ChatContactList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatContactList()
+}
---
+++ Chat/Presentation/Views/PaswordlessLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  PaswordlessLoginView.swift
+//  Chat
+//
+//  Created by Miguel Alejandro Correa Avila on 27/11/23.
+//
+
+import SwiftUI
+
+struct PaswordlessLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaswordlessLoginView()
+}
