432924fd8d481c6d3f30709497ad4f455b882a33
Migalejo <migalejo85@gmail.com>
2023-11-27 16:00:00 +0000

    check user registration to db on login

---
+++ Chat/Data/Service/RegistrationService.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationService.swift
+//  Chat
+//
+//  Created by Miguel Alejandro Correa Avila on 27/11/23.
+//
+
+import Foundation
---
+++ Chat/Presentation/Views/RegisterView.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterView.swift
+//  Chat
+//
+//  Created by Miguel Alejandro Correa Avila on 27/11/23.
+//
+
+import Foundation
